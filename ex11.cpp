c228de1d99da3179a38f65d71ef74a0ca3a74efc
Peer O'Joke <peer.o.joke@gmail.com>
2021-11-03 14:50:57 +0200

    ex11, examples in main()

--- ex11.cpp
+++ ex11.cpp
@@ -93,9 +93,46 @@ Link *Link::advance(int n)
     return p;
 }
 
+void print_all(Link *p)
+{
+    cout << "{ ";
+    while (p)
+    {
+        cout << p->value;
+        if (p = p->next())
+            cout << ", ";
+    }
+    cout << " }\n";
+}
+
 int main()
 try
 {
+    Link *norse_gods = new Link{"Thor"};
+    norse_gods = norse_gods->insert(new Link{"Odin"});
+    norse_gods = norse_gods->insert(new Link{"Zeus"});
+    norse_gods = norse_gods->insert(new Link{"Freia"});
+
+    Link *greek_gods = new Link{"Hera"};
+    greek_gods = greek_gods->insert(new Link{"Athena"});
+    greek_gods = greek_gods->insert(new Link{"Mars"});
+    greek_gods = greek_gods->insert(new Link{"Poseidon"});
+
+    Link *p = greek_gods->find("Mars");
+    if (p)
+        p->value = "Ares";
+
+    Link *p2 = norse_gods->find("Zeus");
+    if (p2)
+    {
+        if (p2 == norse_gods)
+            norse_gods = p2->next();
+        p2->erase();
+        greek_gods = greek_gods->insert(p2);
+    }
+
+    print_all(norse_gods);
+    print_all(greek_gods);
 }
 catch (const std::exception &e)
 {
